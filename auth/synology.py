7b5736cb6996e8a264fbed211328d786506e591e
Ryunhee Han <Ryun@Ryunheeui-MacBook-Pro.local>
2014-11-07 02:58:36 +0900

    Rename signin function

--- auth/synology.py
+++ auth/synology.py
@@ -1,14 +1,14 @@
 import os
 import re
 
-def signin(user, password):
+def is_syno_user(user, password):
     result = os.popen('synouser --login %s %s' % (user, password)).read()
     return 'OK' in result
 
 
 def is_group_member(group, user):
-    result = os.popen('synogroup --get %s' % group).read()
-    list = tuple(result.split('\n'))[4:-1]
+    result = os.popen('synogroup --get %s' % group).read().split('\n')[4:-1]
+    list = tuple(result)
     for one in list:
         p = re.compile('\[.*\]')
         if user == p.search(one).group()[1:-1]:

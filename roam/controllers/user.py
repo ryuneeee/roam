58bcde043271cc2b2633188acc21b9add99ca5b7
Ryunhee Han <Ryun@Ryunheeui-MacBook-Pro.local>
2014-11-10 04:58:22 +0900

    Add return statement for redirect

--- roam/controllers/user.py
+++ roam/controllers/user.py
@@ -1,28 +1,27 @@
 from flask import *
 
 from auth.synology import is_syno_user
+from test.fake_syno_shell import FakeSyno
 
 bp = Blueprint('user', __name__, template_folder='templates')
+
 
 @bp.route('/login', methods=['GET'])
 def login_page():
     return render_template('login.html')
 
 
-
 @bp.route('/login', methods=['POST'])
 def login():
-
     user, password = request.form['user'], request.form['password']
 
     if is_syno_user(user=user, password=password):
         session['user'] = user
 
-        resp = make_response(render_template('index.html'))
-        redirect(url_for('index_page'))
+        return redirect(url_for('index_page'))
 
     else:
-        #TODO: Print login error mszg.
+        # TODO: Print login error msg.
         resp = make_response(render_template('login.html'))
 
     return resp

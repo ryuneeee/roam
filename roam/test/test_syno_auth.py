0e29b8c57258029fcbcd5a311bd02520c99a3613
ryuneeee <ryuneeee@gmail.com>
2015-01-26 03:21:00 +0900

    refactor

--- roam/test/test_syno_auth.py
+++ roam/test/test_syno_auth.py
@@ -10,17 +10,17 @@ class synoAuth(unittest.TestCase):
         self.syno = FakeSyno(user='ryun', password='secret')
 
     def test_login(self):
-        assert True == is_syno_user('ryun', 'secret')
-        assert False == is_syno_user('ryun', 'hello')
-        assert False == is_syno_user('not user', 'not user')
+        self.assertTrue(is_syno_user('ryun', 'secret'))
+        self.assertFalse(is_syno_user('ryun', 'hello'))
+        self.assertFalse(is_syno_user('not user', 'not user'))
 
     def test_group(self):
         self.syno.add_user_to_group('ryun', 'ryunbox')
         self.syno.add_user('ryuneeee', 'secret2', 'ryunbox')
-        assert True == is_group_member(user='ryun', group='ryunbox')
-        assert True == is_group_member(user='ryuneeee', group='ryunbox')
-        assert False == is_group_member('ryunbox', 'empty')
-        assert False == is_group_member('empty', 'ryun')
+        self.assertTrue(is_group_member(user='ryun', group='ryunbox'))
+        self.assertTrue(is_group_member(user='ryuneeee', group='ryunbox'))
+        self.assertFalse(is_group_member('ryunbox', 'empty'))
+        self.assertFalse(is_group_member('empty', 'ryun'))
 
     def tearDown(self):
         super().tearDown()

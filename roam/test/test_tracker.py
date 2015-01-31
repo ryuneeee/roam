754f6234943ba3ef7bbfd5bed00e91e02590e43f
ryuneeee <ryuneeee@gmail.com>
2015-01-31 17:06:38 +0900

    Dynamically Loading modules in TrackerManager

--- roam/test/test_tracker.py
+++ roam/test/test_tracker.py
@@ -10,23 +10,42 @@ class testTracker(unittest.TestCase):
     def setUp(self):
         super().setUp()
 
-    def test_get_lists(self):
         curdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+'/trackers'
         os.chdir(curdir)
 
         # Make dummy file
         open('dummy_tracker.py', 'a').close()
+        open('dummy_tracker_two.py', 'a').close()
+
+
+    def test_file_lists(self):
+        tmgr = manager.TrackerManager()
+        list = tmgr.file_lists()
+        self.assertIn('dummy_tracker.py', list)
+        self.assertIn('dummy_tracker_two.py', list)
+
+
+    def test_load_module(self):
+        names = []
 
         tmgr = manager.TrackerManager()
-        list = tmgr.get_lists()
-        self.assertIn('dummy_tracker.py', list)
+        trackers = tmgr.load_modules()
 
-        # Remove dummy file
-        os.remove('dummy_tracker.py')
-        self.assertFalse(os.path.isfile('dummy_tracker.py'))
+        for tracker in trackers:
+            names.append(tracker.__name__)
+
+        self.assertIn('trackers.dummy_tracker', names)
+        self.assertIn('trackers.dummy_tracker_two', names)
+
 
 
     def tearDown(self):
+        # Remove dummy file
+        os.remove('dummy_tracker.py')
+        os.remove('dummy_tracker_two.py')
+        self.assertFalse(os.path.isfile('dummy_tracker.py'))
+        self.assertFalse(os.path.isfile('dummy_tracker_two.py'))
+
         super().tearDown()
 
 if __name__ == '__main__':
--- roam/trackers/manager.py
+++ roam/trackers/manager.py
@@ -1,11 +1,14 @@
+import importlib
+
 __author__ = 'Ryun'
 
 from glob import glob
 import os
 
+
 class TrackerManager():
 
-    def get_lists(self):
+    def file_lists(self):
         curdir = os.path.dirname(os.path.abspath(__file__))
         os.chdir(curdir)
 
@@ -13,8 +16,14 @@ class TrackerManager():
         trackers.remove(os.path.basename(__file__))
         return trackers
 
-    # TODO: Save trackers into dict after import python modules and make Tracker obj.
+    # TODO: Save trackers into dict or array after import python modules and make Tracker obj.
     # Import modules: https://github.com/storyhe/playWithBot/blob/master/slask.py#L31
-    def load_all(self):
-        trackers = self.get_lists()
-        return trackers
+    def load_modules(self):
+        trackers = self.file_lists()
+
+        loaded = []
+
+        for tracker in trackers:
+            loaded.append(importlib.import_module('trackers.'+tracker[:-3]))
+
+        return loaded
